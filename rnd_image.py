b8ad2c7ef552f40acfece48718e9bcebc2b47a04
Tom <tomburgess24@gmail.com>
2025-06-25 08:31:29 +0100

    Delete rnd_image.py

--- rnd_image.py
+++
@@ -1,8 +0,0 @@
-import random
-def getRndFile():
-	note = random.randint(1,7)
-	notes = ["A", "B", "C", "D", "E", "F", "G"]
-	octave = random.randint(1,3)
-	filename = notes[note-1] + str(octave) + ".png"
-	return filename
-
